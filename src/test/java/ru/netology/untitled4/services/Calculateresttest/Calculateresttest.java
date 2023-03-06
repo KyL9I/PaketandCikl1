0775697ef017f062b554ebeee18f6fe44f61fbf6
Alex test <alexkulikov09@mail.ru>
2023-03-06 19:17:10 +0300

    rest.csv

--- src/test/java/ru/netology/untitled4/services/Calculateresttest/Calculateresttest.java
+++ src/test/java/ru/netology/untitled4/services/Calculateresttest/Calculateresttest.java
@@ -2,28 +2,22 @@ package ru.netology.untitled4.services.Calculateresttest;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import org.junit.jupiter.params.provider.CsvSource;
 import ru.netology.untitled4.services.Calculaterest;
 
 
 public class Calculateresttest {
 
-    @Test
-    public void testCalculateMonthRest1() {
+    @ParameterizedTest
+    @CsvFileSource(files = "src/test/resources/rest.csv")
+    public void testCalculateMonthRest(int expected, int income, int expenses, int threshold) {
         Calculaterest service = new Calculaterest();
 
-        int expected = 3;
-        int actual = service.calculate(10_000, 3000, 20_000);
+        int actual = service.calculate(income, expenses, threshold);
 
         Assertions.assertEquals(expected, actual);
     }
 
-    @Test
-    public void testCalculateMonthRest2() {
-        Calculaterest service = new Calculaterest();
-
-        int expected = 2;
-        int actual = service.calculate(100_000, 60_000, 150_000);
-
-        Assertions.assertEquals(expected, actual);
-    }
 }
